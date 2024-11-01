5d2a01afb542ead90f8f8c3a8a2f4f34d5f3355d
𝓖𝓸𝓴𝓾𝓵𝓪𝓴𝓪𝓷𝓷𝓪𝓷 <gokulakannanp2024@gmail.com>
2024-11-01 18:00:20 +0530

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,249 +1,319 @@
-#*****************************#*****************************#*****************************#
-#                             #                                                           #
-#     Tkinter  Calculator     #                                                           #
-#                             #                                                           #
-#                             #                                                           #
-#*****************************#                          WELCOME                          #
-#                             #                                                           #
-#    Gokulakannan P           #                                                           #
-#    Python Programmer        #                                                           #
-#                             #                                                           #
-#*****************************#*****************************#*****************************#
-
-
-from tkinter import *
-from tkinter.font import Font
+from customtkinter import *
 from tkinter import messagebox
 
-root = Tk()
-root.state("zoomed")
+root = CTk()
 root.title("Calculator")
-root.configure(border=5)
-
-c = StringVar()
-
-
-font1 = Font(root, family="Ariel", size=40, weight="bold")
+root.geometry("350x530")
+root.resizable(False, False)
+icon_path = os.path.join(os.path.dirname(__file__), "calc.ico")
+root.iconbitmap(icon_path)
 
 
-frame1 = Frame(root, bg="light green")
-frame1.pack(fill=X, side=TOP)
+def update(value):
+    entry1.insert(END, curent_value + value)
 
 
-label1 = Label(frame1, text="", font=("sans-serif", 89, "bold"), bg="Light green")
-label1.grid(row=0, column=0, columnspan=9)
+def b1():
+    update("1")
 
 
-enter =Entry(frame1, font=font1, textvariable=c, width=52)
-enter.configure(justify="right", relief=SOLID)
-enter.grid(row=1, column=0, padx=6)
+def b2():
+    update("2")
 
 
-def calculate():
-    try:
-         result = eval(c.get())
-         c.set("")
-         c.set(result)
-    except Exception as e:
-        c.set("")
-        messagebox.showerror("Syntax Error", "Invalid syntax")
+def b3():
+    update("3")
 
 
-def backspace(event):
-    if event.keysym == "BackSpace":
-        entry_text = c.get()
-        entry_text = entry_text[:-1]
-        c.delete(0, END)
-        c.insert(0, entry_text)
+def b4():
+    update("4")
 
 
-
-current_value = enter.get()
-enter.delete(0, END)
-
-def update_entry(value):
-    enter.insert(END,current_value + value)
+def b5():
+    update("5")
 
 
-def _1():
-    update_entry("1")
+def b6():
+    update("6")
 
 
-def _2():
-    update_entry("2")
+def b7():
+    update("7")
 
 
-def _3():
-    update_entry("3")
+def b8():
+    update("8")
 
 
-def _4():
-    update_entry("4")
+def b9():
+    update("9")
 
 
-def _5():
-    update_entry("5")
+def bm():
+    update("*")
 
 
-def _6():
-    update_entry("6")
+def bmi():
+    update("-")
 
 
-def _7():
-    update_entry("7")
+def bp():
+    update("+")
 
 
-def _8():
-    update_entry("8")
+def bd():
+    update("/")
 
 
-def _9():
-    update_entry("9")
-
-def _0():
-    update_entry("0")
+def bdot():
+    update(".")
 
 
-def _subraction():
-    update_entry("-")
-
-
-def _divide():
-    update_entry("/")
-
-
-def _add():
-    update_entry("+")
-
-
-def _multiplication():
-    update_entry("*")
+def b0():
+    update("0")
 
 
 def _backspace():
-    entry_text = enter.get()
+    entry_text = entry1.get()
     entry_text = entry_text[:-1]
-    enter.delete(0, END)
-    enter.insert(0, entry_text)
+    entry1.delete(0, END)
+    entry1.insert(0, entry_text)
 
 
-def _clear():
-    c.set("")
-
-
-def _equal():
+def result():
     try:
-         result = eval(c.get())
-         c.set("")
-         c.set(result)
+        temp = entry1.get()
+        temp1 = eval(temp)
+        entry1.delete(0, END)
+        entry1.insert(0, temp1)
     except Exception as e:
-        c.set("")
-        messagebox.showerror("Syntax Error", "Invalid syntax")
+        entry1.delete(0, END)
+        messagebox.showerror("Error", "Invalid input")
 
 
+frame1 = CTkFrame(root,
+                  fg_color="azure",
+                  height=530,
+                  width=350,
+                  corner_radius=0)
+frame1.place(x=0, y=0)
 
-frame2 = Frame(frame1, bg="light green", border=3)
-frame2.grid(row=2, column=0)
+entry1 = CTkEntry(frame1,
+                  height=70,
+                  width=330,
+                  corner_radius=1,
+                  border_width=1,
+                  font=("Arial Black", 34, "bold"))
+entry1.place(x=10, y=20)
+curent_value = entry1.get()
 
-button1 = Button(frame2, text="1", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_1)
-button1.config(relief=SOLID)
-button1.grid(row=0, column=0, sticky=E, padx=10, pady=10)
+button_7 = CTkButton(frame1,
+                     height=70,
+                     width=75,
+                     text="7",
+                     font=("Arial Black", 28, "bold"),
+                     corner_radius=4,
+                     fg_color="ivory4",
+                     text_color="black",
+                     hover_color="ivory3",
+                     command=b7)
+button_7.place(x=10, y=110)
 
-button2 =Button(frame2, text="2", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_2)
-button2.config(relief=SOLID)
-button2.grid(row=0, column=1, sticky=E, padx=10, pady=10)
+button_8 = CTkButton(frame1,
+                     height=70,
+                     width=75,
+                     text="8",
+                     font=("Arial Black", 28, "bold"),
+                     corner_radius=4,
+                     fg_color="ivory4",
+                     text_color="black",
+                     hover_color="ivory3",
+                     command=b8)
+button_8.place(x=95, y=110)
 
+button_9 = CTkButton(frame1,
+                     height=70,
+                     width=75,
+                     text="9",
+                     font=("Arial Black", 28, "bold"),
+                     corner_radius=4,
+                     fg_color="ivory4",
+                     text_color="black",
+                     hover_color="ivory3",
+                     command=b9)
+button_9.place(x=95 + 85, y=110)
 
-button3 = Button(frame2, text="3", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_3)
-button3.config(relief=SOLID)
-button3.grid(row=0, column=2, sticky=E, padx=10, pady=10)
+button_mul = CTkButton(frame1,
+                       height=70,
+                       width=75,
+                       text="X",
+                       font=("Arial Black", 28, "bold"),
+                       corner_radius=4,
+                       fg_color="ivory4",
+                       text_color="black",
+                       hover_color="ivory3", command=bm)
+button_mul.place(x=95 + 85 + 85, y=110)
 
+# row 2
 
-button4 = Button(frame2, text="4", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_4)
-button4.config(relief=SOLID)
-button4.grid(row=0, column=3, sticky=E, padx=10, pady=10)
+button_4 = CTkButton(frame1,
+                     height=70,
+                     width=75,
+                     text="4",
+                     font=("Arial Black", 28, "bold"),
+                     corner_radius=4,
+                     fg_color="ivory4",
+                     text_color="black",
+                     hover_color="ivory3",
+                     command=b4)
+button_4.place(x=10, y=110 + 85)
 
+button_5 = CTkButton(frame1,
+                     height=70,
+                     width=75,
+                     text="5",
+                     font=("Arial Black", 28, "bold"),
+                     corner_radius=4,
+                     fg_color="ivory4",
+                     text_color="black",
+                     hover_color="ivory3",
+                     command=b5)
+button_5.place(x=95, y=110 + 85)
 
-button5 = Button(frame2, text="5", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_5)
-button5.config(relief=SOLID)
-button5.grid(row=1, column=0, sticky=E, padx=10, pady=10)
+button_6 = CTkButton(frame1,
+                     height=70,
+                     width=75,
+                     text="6",
+                     font=("Arial Black", 28, "bold"),
+                     corner_radius=4,
+                     fg_color="ivory4",
+                     text_color="black",
+                     hover_color="ivory3",
+                     command=b6)
+button_6.place(x=95 + 85, y=110 + 85)
 
+button_div = CTkButton(frame1,
+                       height=70,
+                       width=75,
+                       text="÷",
+                       font=("Arial Black", 28, "bold"),
+                       corner_radius=4,
+                       fg_color="ivory4",
+                       text_color="black",
+                       hover_color="ivory3",
+                       command=bd)
+button_div.place(x=95 + 85 + 85, y=110 + 85)
 
-button6 = Button(frame2, text="6", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_6)
-button6.config(relief=SOLID)
-button6.grid(row=1, column=1, sticky=E, padx=10, pady=10)
+# row 3
 
+button_1 = CTkButton(frame1,
+                     height=70,
+                     width=75,
+                     text="1",
+                     font=("Arial Black", 28, "bold"),
+                     corner_radius=4,
+                     fg_color="ivory4",
+                     text_color="black",
+                     hover_color="ivory3",
+                     command=b1)
+button_1.place(x=10, y=110 + 85 + 85)
 
-button7 = Button(frame2, text="7", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_7)
-button7.config(relief=SOLID)
-button7.grid(row=1, column=2, sticky=E, padx=10, pady=10)
+button_2 = CTkButton(frame1,
+                     height=70,
+                     width=75,
+                     text="2",
+                     font=("Arial Black", 28, "bold"),
+                     corner_radius=4,
+                     fg_color="ivory4",
+                     text_color="black",
+                     hover_color="ivory3",
+                     command=b2)
+button_2.place(x=95, y=110 + 85 + 85)
 
+button_3 = CTkButton(frame1,
+                     height=70,
+                     width=75,
+                     text="3",
+                     font=("Arial Black", 28, "bold"),
+                     corner_radius=4,
+                     fg_color="ivory4",
+                     text_color="black",
+                     hover_color="ivory3",
+                     command=b3)
+button_3.place(x=95 + 85, y=110 + 85 + 85)
 
-button8 = Button(frame2, text="8", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_8)
-button8.config(relief=SOLID)
-button8.grid(row=1, column=3, sticky=E, padx=10, pady=10)
+button_sub = CTkButton(frame1,
+                       height=70,
+                       width=75,
+                       text="-",
+                       font=("Arial Black", 30, "bold"),
+                       corner_radius=4,
+                       fg_color="ivory4",
+                       text_color="black",
+                       hover_color="ivory3",
+                       command=bmi)
+button_sub.place(x=95 + 85 + 85, y=110 + 85 + 85)
 
+# row 4
 
-button9 = Button(frame2, text="9", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_9)
-button9.config(relief=SOLID)
-button9.grid(row=2, column=0, sticky=E, padx=10, pady=10)
+button_0 = CTkButton(frame1,
+                     height=70,
+                     width=75,
+                     text="0",
+                     font=("Arial Black", 28, "bold"),
+                     corner_radius=4,
+                     fg_color="ivory4",
+                     text_color="black",
+                     hover_color="ivory3",
+                     command=b0)
+button_0.place(x=10, y=110 + 85 + 85 + 85)
 
+button_dot = CTkButton(frame1,
+                       height=70,
+                       width=75,
+                       text=".",
+                       font=("Arial Black", 28, "bold"),
+                       corner_radius=4,
+                       fg_color="ivory4",
+                       text_color="black",
+                       hover_color="ivory3",
+                       command=bdot)
+button_dot.place(x=95, y=110 + 85 + 85 + 85)
 
-button0 = Button(frame2, text="0", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_0)
-button0.config(relief=SOLID)
-button0.grid(row=2, column=1, sticky=E, padx=10, pady=10)
+button_plus = CTkButton(frame1,
+                        height=70,
+                        width=75,
+                        text="+",
+                        font=("Arial Black", 30, "bold"),
+                        corner_radius=4,
+                        fg_color="ivory4",
+                        text_color="black",
+                        hover_color="ivory3",
+                        command=bp)
+button_plus.place(x=95 + 85, y=110 + 85 + 85 + 85)
 
+button_back = CTkButton(frame1,
+                        height=70,
+                        width=75,
+                        text="\u232B",
+                        font=("Arial Black", 30, "bold"),
+                        corner_radius=4,
+                        fg_color="ivory4",
+                        text_color="black",
+                        hover_color="ivory3",
+                        command=_backspace)
+button_back.place(x=95 + 85 + 85, y=110 + 85 + 85 + 85)
 
-buttonadd = Button(frame2, text="+", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_add)
-buttonadd.config(relief=SOLID)
-buttonadd.grid(row=2, column=2, sticky=E, padx=10, pady=10)
-
-buttonsub = Button(frame2, text="-", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_subraction)
-buttonsub.config(relief=SOLID)
-buttonsub.grid(row=2, column=3, sticky=E, padx=10, pady=10)
-
-
-buttonmulti = Button(frame2, text="X", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_multiplication)
-buttonmulti.config(relief=SOLID)
-buttonmulti.grid(row=3, column=0, sticky=E, padx=10, pady=10)
-
-
-buttondivide = Button(frame2, text="/", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_divide)
-buttondivide.config(relief=SOLID)
-buttondivide.grid(row=3, column=1, sticky=E, padx=10, pady=10)
-
-buttonback = Button(frame2, text="Backspase", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_backspace)
-buttonback.config(relief=SOLID)
-buttonback.grid(row=3, column=2, sticky=E, padx=10, pady=10)
-
-
-buttonclear = Button(frame2, text="CE", width=30, height=5, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_clear)
-buttonclear.config(relief=SOLID)
-buttonclear.grid(row=3, column=3, sticky=E, padx=10, pady=10)
-
-
-buttonequal = Button(frame2, text="=", height=5, width=132, bg="grey", fg="BLACK", border=3,
-                activebackground="light grey", activeforeground="Black", font=("ariel black", 10, "bold"), command=_equal)
-buttonequal.config(relief=SOLID)
-buttonequal.grid(row=4, column=0, columnspan=4, padx=10, pady=10)
-
-enter.bind("<Return>", lambda event: calculate())
-enter.bind("<KeyPress>", backspace)
+button_equal = CTkButton(frame1,
+                         height=70,
+                         width=330,
+                         text="=",
+                         font=("Arial Black", 30, "bold"),
+                         corner_radius=4,
+                         fg_color="ivory4",
+                         text_color="black",
+                         hover_color="ivory3",
+                         command=result)
+button_equal.place(x=10, y=110 + 85 + 85 + 85 + 85)
 
 root.mainloop()
